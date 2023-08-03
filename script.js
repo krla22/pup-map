887ac059536ee71391f93fff03e5ecacf69f72e6
krla22 <kurtrobin22@gmail.com>
2023-08-03 23:53:30 +0800

    PUP Map

---
+++ script.js
@@ -0,0 +1,492 @@
+function showOval() {
+    let pupBG  = document.getElementById('pup-map')
+    let map = document.getElementById('oval')
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    } 
+}
+function resetOval() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('oval');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"]="blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showChapel() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('chapel');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetChapel() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('chapel');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showFood() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('food');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetFood() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('food');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showTheater() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('theater');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetTheater() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('theater');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+
+/** */
+
+function showMabini() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('mabini');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetMabini() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('mabini');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showCharlie() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('charlie');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetCharlie() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('charlie');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showEastw() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('eastw');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetEastw() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('eastw');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showEntrance() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('entrance');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetEntrance() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('entrance');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showGuard() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('guard');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetGuard() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('guard');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showLagoon() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('lagoon');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetLagoon() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('lagoon');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showNinoy() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('ninoy');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetNinoy() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('ninoy');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showNorthw() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('northw');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetNorthw() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('northw');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showObelisk() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('obelisk');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetObelisk() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('obelisk');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showPhysical() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('physical');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetPhysical() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('physical');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showSouthnwest() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('southnwest');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetSouthnwest() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('southnwest');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showPool() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('pool');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetPool() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('pool');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showTennisbball() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('tennisbball');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetTennisbball() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('tennisbball');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+/** */
+
+function showVisitor() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('visitor');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 0) {
+        map.style.opacity = 100;
+        map.style.zIndex = 1;
+        pupBG.style["filter"]="blur(3px)";
+        button.style.opacity = 0;
+    }
+}
+function resetVisitor() {
+    let pupBG  = document.getElementById('pup-map');
+    let map = document.getElementById('visitor');
+    let button = document.getElementById('button-div')
+
+    if (map.style.opacity == 100) {
+        map.style.opacity = 0;
+        map.style.zIndex = 0;
+        pupBG.style["filter"] = "blur(0px)";
+        button.style.opacity = 100;
+    }
+}
+
+function toActual() {
+    document.getElementById("pup-map").style.backgroundImage = "url(https://cdn.discordapp.com/attachments/1048190439557181474/1048496883120738384/PUP_Full_Map.png)";
+}
+
+function toDrawn() {
+    document.getElementById("pup-map").style.backgroundImage = "url(https://cdn.discordapp.com/attachments/1048190439557181474/1048190502941503528/PUP_Drawn_Map.png)";
+}
